98c5dfc39687a4500819f3984b7e1711886a8f3f
kaushalm <kaushalmehra1987@gmail.com>
2021-02-04 02:09:18 +0530

    fixed 404 page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./style.css";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Home from "./Components/Home";
 import Contact from "./Components/Contact";
 import About from "./Components/About";
@@ -11,12 +11,12 @@ export default function App() {
   return (
     <main>
       <Navbar />
-      <switch>
+      <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
         <Route path="*" component={Error} />
-      </switch>
+      </Switch>
     </main>
   );
 }
